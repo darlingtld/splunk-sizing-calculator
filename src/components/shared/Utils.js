3914aab06ae4dab6dfe70b68eb3807c88b17a185
lingda <lingda@splunk.com>
2017-11-30 16:59:26 +0800

    simplify animation

--- src/components/shared/Utils.js
+++ src/components/shared/Utils.js
@@ -21,5 +21,6 @@ export const getRandomTransitionAnimation = () => {
         'horizontal flip', 'vertical flip', 'drop', 'fly left', 'fly right', 'fly up', 'fly down',
         'swing left', 'swing right', 'swing up', 'swing down', 'browse', 'browse right', 'slide down', 'slide up', 'slide right', 'jiggle',
         'flash', 'shake', 'pulse', 'tada', 'bounce'];
-    return animation[random(animation.length - 1)];
+    // return animation[random(animation.length - 1)];
+    return 'fade';
 }
